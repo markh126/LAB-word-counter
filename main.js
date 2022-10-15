cac266a1aa08a390b952d6aed99c02ce1cb24b48
Mark Hamilton <ham.mark126@gmail.com>
2022-10-14 19:25:58 -0500

    worked on formatting and functionality

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
+function myFunction() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
 console.log("Connected!")
 // YOU WILL MODIFY THIS FUNCTION TO GET THE PROGRAM TO WORK
 const wordCounter = (value) => {
   // Type into the input and press submit, you will see the value you entered in your console
+
   console.log(value);
 
   if (value) {
     // COMPLETE THE LOGIC 
-    wordCount.innerHTML = `Word Count: 0`; 
+    wordCount.innerHTML = `Word count: 0`; 
   } else {
     // if the value is empty, set the error message value to "Please input text"
-    error.innerHTML = ""; // UPDATE THIS
+    error.innerHTML = "Please input text"; // UPDATE THIS
   }
 }
 
